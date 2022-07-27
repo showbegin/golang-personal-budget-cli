31128f11e495f8e5f12f6b66f3234ca496d8e3ea
showbegin <showbegin@gmail.com>
2022-07-27 15:50:05 +0600

    structs added

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,13 @@
 package module1
 
 // Budget stores budget information
+type budget struct {
+	Max   float32
+	Items []Item
+}
 
 // Item stores item information
+type item struct {
+	Description string
+	Price       float32
+}
